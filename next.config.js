b29ef3a4cb3a3553a7b5810e55e40d97bd9a0bf4
marceloroot <marcelo.lima.gomes.23@gmail.com>
2025-03-12 18:23:37 -0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Ignorar erros de build do TypeScript
     ignoreBuildErrors: true,
   },
   reactStrictMode: true,
-  // Desativar completamente o SWC
-  swcMinify: false,
-  webpack: (config, { isServer }) => {
-    // Forçar webpack a usar Babel
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["next/babel"],
-        },
-      },
-    });
-    return config;
-  },
+  swcMinify: true, // Reativando o SWC (melhor performance)
 };
 
 module.exports = nextConfig;
